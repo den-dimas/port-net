413010db1a4464ac90cd2107a88cee34a0c3a831
Den <dimass.drm@gmail.com>
2024-11-06 23:13:53 +0700

    add redirect to dashboard

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,13 +1,21 @@
-import type { MetaFunction } from "@remix-run/node";
+import {
+  redirect,
+  type LoaderFunction,
+  type MetaFunction,
+} from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [{ title: "PortNet | Traffic Monitoring" }];
 };
 
+export const loader: LoaderFunction = async ({ request }) => {
+  return redirect("/dashboard");
+};
+
 export default function Index() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <div>apsiodjoawijdoiajs</div>
+      <div>Redirecting...</div>
     </div>
   );
 }
